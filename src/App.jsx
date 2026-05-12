a46b892dd1dd8814b8c543ccedc1694e08d81a84
Santi Baca Castex <santi@agroneros.com>
2026-05-12 00:18:57 +0000

    Fix: remover imports de Google Maps en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
-import { GoogleMap, LoadScript, MarkerF, InfoWindowF } from '@react-google-maps/api'
 import { signInAnonymously } from 'firebase/auth'
-import { db, auth } from './firebase-config'
+import { auth } from './firebase-config'
 import PaginaMapa from './pages/PaginaMapa'
 import './App.css'
 
